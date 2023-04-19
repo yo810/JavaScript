705cf4fd4b978005e46ab4e9071283e92cd854e4
EC2 Default User <ec2-user@ip-172-31-45-228.ap-northeast-1.compute.internal>
2023-04-19 21:31:53 +0000

    add 文法

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-alert('Hello World');
+let hello = 'Hello World';
+alert(hello);
+
+let int1 = 1;
+alert(int1)
+
+// 負数を代入する
+let int2 = -10;
+alert(int2)
+
+// 小数点を代入する
+let float1 = 3.14;
+alert(float1)
+
+// 文字列を代入する
+let str1 = 'JavaScriptを覚えよう';
+alert(str1)
+
+// 足し算
+alert(4 + 3);
+
+// 引き算
+alert(8 - 5);
+
+// 掛け算
+alert(2 * 6);
+
+// 割り算
+alert(10 / 2);
+
+alert('Hello' + 'World')
+
+let str3 = 'Hello';
+let str4 = 'World!!';
+alert(str3 + str4);
