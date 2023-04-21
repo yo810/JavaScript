69657bb5160661064980c8316f98f00ee3b3bc9c
EC2 Default User <ec2-user@ip-172-31-45-228.ap-northeast-1.compute.internal>
2023-04-21 20:54:44 +0000

    add 条件分岐 繰り返し処理

--- app.js
+++ app.js
@@ -33,3 +33,34 @@ alert('Hello' + 'World')
 let str3 = 'Hello';
 let str4 = 'World!!';
 alert(str3 + str4);
+
+let orange = 100;
+let apple = 120;
+
+if(orange < apple){
+  alert('みかんの値段がりんごより安い');
+} else if(orange == apple){
+  alert('みかんとりんごが同じ値段');
+} else{
+  alert('みかんの値段がりんごより高い');
+}
+
+let max = 1000;
+let num = 5;
+let count = 0;
+
+while(num < max){
+  num = num * 3;
+  count = count + 1;
+}
+
+alert('3を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+
+let i;
+let number = 0;
+
+for(i = 1; i < 11; i++){
+  number = number + i;
+}
+
+alert('1から10まで足し算した結果は' + number + 'です');
